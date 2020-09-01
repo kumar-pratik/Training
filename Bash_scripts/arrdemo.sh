eab9eccc2d4640794cc0e2b6b56ef9ab2e2270c1
Kumar Pratik <kumar.pratik@knoldus.com>
2020-09-01 11:39:13 +0530

    Added comments

--- Bash_scripts/arrdemo.sh
+++ Bash_scripts/arrdemo.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+#
+# SCRIPT: arrdemo.sh
+# AUTHOR: Kumar Pratik
+# DATE:   31/08/2020
+# REV:    1.1.A (Valid are A, B, D, T and P)
+#          (For Alpha, Beta, Dev, Test and Production)
+#
+#
+# PLATFORM: Ubuntu
+# 
+# PURPOSE: Demo on how to create, add, delete and filter an array
+# REV LIST:
+#    DATE        : 01/09/2020
+#    BY          : Kumar Pratik
+#    MODIFICATION: Added Comments
+# 
+# set -n   # Uncomment to check script syntax, without execution.
+#          # NOTE: Do forget comment it back as it won't allow the 
+#          # the script to execute.
+#
+# set +x   # Uncomment this for debugging this shell script.
+#
+#
+################################################################
 #Ways of array declaration
 fruits[0]=apple
 fruits[1]=oranges
@@ -11,6 +35,7 @@ declare -a unix=("Red Hat" "Fedora" "Debian")
 
 student=(Pratik John Richa Raunak)
 
+################################################################
 #Ways to print array
 echo "Fruits are: ${fruits[@]}"
 echo "Unix are: ${unix[@]}"
